ad006810d3327e4c8b91320e4181e264ffdc83c1
mxj92 <1016706520@qq.com>
2019-08-04 16:11:29 +0800

    modify TestHttpServerHandler

--- src/main/java/com/my/netty/first/TestHttpServerHandler.java
+++ src/main/java/com/my/netty/first/TestHttpServerHandler.java
@@ -10,7 +10,7 @@ import io.netty.util.CharsetUtil;
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        if (msg instanceof HttpObject) {
+        if (msg instanceof HttpRequest) {
             ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");

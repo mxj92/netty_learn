7db563ab16842886d4e0d390d0e13a8979708865
mxj92 <1016706520@qq.com>
2020-01-14 21:15:09 +0800

    commit httpserver handler

--- src/main/java/com/my/netty/first/TestHttpServerHandler.java
+++ src/main/java/com/my/netty/first/TestHttpServerHandler.java
@@ -7,11 +7,20 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
         System.out.println("channel read0");
         if (msg instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest)msg;
+            System.out.println("请求方法名："+httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求favicon");
+                return;
+            }
             ByteBuf content = Unpooled.copiedBuffer("hello world", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
